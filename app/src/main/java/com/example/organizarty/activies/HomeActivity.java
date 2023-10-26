f5762baa3cc171ed92ec9034b7c83b01b985d226
3JD <3jd@bglab.local>
2023-10-26 08:49:24 -0300

    Iniciando a tela home

--- app/src/main/java/com/example/organizarty/activies/HomeActivity.java
+++ app/src/main/java/com/example/organizarty/activies/HomeActivity.java
@@ -1,19 +1,16 @@
 package com.example.organizarty.activies;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.widget.Button;
+
 import com.example.organizarty.R;
 
-public class LoginActivity extends AppCompatActivity {
-
-    Button entrar;
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_home);
     }
-
-
 }
