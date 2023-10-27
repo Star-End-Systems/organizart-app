d14dc1c2eac2ec5cc0a02b499d087e66d560f030
Débora Liberato <dliberato014@gmail.com>
2023-10-26 21:30:50 -0300

    Criando o hearder da tela home e ajustando detalhes na tela de login

--- app/src/main/java/com/example/organizarty/activies/LoginActivity.java
+++ app/src/main/java/com/example/organizarty/activies/LoginActivity.java
@@ -1,17 +1,32 @@
 package com.example.organizarty.activies;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageButton;
+
 import com.example.organizarty.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button entrar;
+    ImageButton SingIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SingIn = findViewById(R.id.btn_sing);
+
+        SingIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent logar = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(logar);
+            }
+        });
 
     }
 
