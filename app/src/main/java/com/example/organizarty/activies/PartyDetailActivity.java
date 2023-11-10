212296af9d882db159a3c66e0d0091cd4f964b3e
lu-css <dossantossilvalucas963@gmail.com>
2023-11-09 22:15:13 -0300

    party details page

---
+++ app/src/main/java/com/example/organizarty/activies/PartyDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.organizarty.activies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.organizarty.R;
+
+public class PartyDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_party_detail);
+    }
+}
