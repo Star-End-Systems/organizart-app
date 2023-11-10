7314c06e952efb9707a95ee278cbe6c678fd4a7c
lu-css <dossantossilvalucas963@gmail.com>
2023-11-10 17:49:30 -0300

    debora cade voce

---
+++ app/src/main/java/com/example/organizarty/activies/YourOrders.java
@@ -0,0 +1,16 @@
+package com.example.organizarty.activies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.organizarty.R;
+
+public class YourOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_orders);
+    }
+}
--- app/src/main/java/com/example/organizarty/activies/app/main/views/activities/HomeActivity.java
+++ app/src/main/java/com/example/organizarty/activies/app/main/views/activities/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.organizarty.activies.app.main.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import android.os.Bundle;
@@ -8,6 +11,9 @@ import android.widget.GridView;
 import android.widget.LinearLayout;
 
 import com.example.organizarty.R;
+import com.example.organizarty.activies.LoginActivity;
+import com.example.organizarty.activies.MainActivity;
+import com.example.organizarty.activies.PartyDetailActivity;
 import com.example.organizarty.activies.app.main.views.adapters.OrderAdapter;
 import com.example.organizarty.activies.app.main.views.adapters.OrderCard;
 
@@ -29,6 +35,11 @@ public class HomeActivity extends AppCompatActivity {
 
     private void setupCards(){
 
+    }
+
+    public void goToDescriptionParty(View view){
+        Intent intent = new Intent(this, PartyDetailActivity.class);
+        startActivity(intent);
     }
 
     private void goToDetailsDescriptionActivity(OrderCard o){
